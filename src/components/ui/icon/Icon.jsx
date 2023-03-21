4a47d12da75c75db3b3517a1a9aa99fc7c408dad
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-21 10:54:40 +0200

    Icon uzd

---
+++ src/components/ui/icon/Icon.jsx
@@ -0,0 +1,7 @@
+/* 
+sukurti ikonele comppnenta, panaudoti isitikinti kad veikia
+parasius <Icon icon='shower' /> 
+turetume gauti <i class='fa fa-shower' aria-hidden='true'></i>
+
+extra <Icon icon='shower' lg  gausim didele ikonele/>
+*/
